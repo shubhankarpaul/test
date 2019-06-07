c0850ee19679cff36cf616e71a5a757b011aa294
Shubhankar Paul <shupaul@publicisgroupe.net>
2019-06-07 16:10:36 +0530

    i

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -i ./ssh.pem root@35.200.236.225 "echo "you are in"; hostname;apt update;apt upgrade -y;apt install apache2 -y;a2enmod ssl;a2enmod rewrite;a2enmod headers; systemctl restart apache2"
+ssh root@35.200.236.225 "echo "you are in"; hostname;apt update;apt upgrade -y;apt install apache2 -y;a2enmod ssl;a2enmod rewrite;a2enmod headers; systemctl restart apache2"
