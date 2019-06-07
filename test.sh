e3645a9befbf250c6dbf1d24600d8278f6d13c19
Shubhankar Paul <shupaul@publicisgroupe.net>
2019-06-07 15:14:39 +0530

    "file change"

--- test.sh
+++ test.sh
@@ -1,9 +1,3 @@
-echo "Testing..."
-sleep 1
-echo "1 sec"
-sleep 1
-echo "2 sec"
-sleep 1
-echo "3 sec"
+#!/bin/bash
 
-echo "Done!"
+sshpass -p GF2rq6XPdgES4TwF ssh -o StrictHostKeyChecking=no root@35.200.236.225 "echo "you are in"; hostname;apt update;apt upgrade -y"sshpass -p GF2rq6XPdgES4TwF ssh -o StrictHostKeyChecking=no root@35.200.236.225 "echo "you are in"; hostname;apt update;apt upgrade -y;apt install apache2 -y;a2enmod ssl;a2enmod rewrite;a2enmod headers; systemctl restart apache2"
