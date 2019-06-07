4e0a1a66af5f035fe512f35a93e731c298bef2e5
Shubhankar Paul <shupaul@publicisgroupe.net>
2019-06-07 15:36:17 +0530

    d

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 apt update -y
-apt-get update -y
-apt-get install ssh-client -y
+apt install ssh-client -y
 ssh -i ./ssh.pem root@35.200.236.225 "echo "you are in"; hostname;apt update;apt upgrade -y;apt install apache2 -y;a2enmod ssl;a2enmod rewrite;a2enmod headers; systemctl restart apache2"
