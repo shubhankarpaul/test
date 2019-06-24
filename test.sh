c3e47f7043051986da9ae0fe91e2ac1bbcd70070
shubhankarpaul <shubhankarpaul@outlook.com>
2019-06-24 09:46:59 +0000

    test1

--- test.sh
+++ test.sh
@@ -1,2 +1,1 @@
 #!/bin/bash
-ssh root@35.200.236.225 "echo "you are in"; hostname;apt update;apt upgrade -y;apt install apache2 -y;a2enmod ssl;a2enmod rewrite;a2enmod headers; systemctl restart apache2"
