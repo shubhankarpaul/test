b1c1a73928582c8f178ba206649b51ae00ed6e0b
shubhankarpaul <shubhankarpaul@outlook.com>
2019-06-28 04:38:34 +0000

    test2

---
+++ src/main/java/com/try2answer/drone/App.java
@@ -0,0 +1,13 @@
+package com.try2answer.drone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
