4fbf8e02e9c431478b9b853649e21c2e4ac5941c
shubhankarpaul <shubhankarpaul@outlook.com>
2019-06-28 06:57:13 +0000

    test2

--- test.sh
+++ test.sh
@@ -11,4 +11,18 @@ mvn clean install
 #mvn build
 ls -la
 artifact=`find . -name "*.jar" -print0|xargs -r -0 ls -1 -t|head -1`
+
+echo "Current Snapshot Version"
+
+mvn -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive exec:exec -q
+echo ""
+echo "Changing The Snapshote Version"
+echo ""
+
+mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT
+echo ""
+echo "Snapshot version after change"
+echo ""
+mvn -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive exec:exec -q
+echo ""
 echo $artifact
