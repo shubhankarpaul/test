fe070bcaed9dc67d97dc75631aae4a0cff529ad8
shubhankarpaul <shubhankarpaul@outlook.com>
2019-06-28 04:45:54 +0000

    test2

--- test.sh
+++ test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 mvn -v
 pwd
-mvn -Dmaven.javadoc.skip=true verify && mvn compile
+mvn compile
 echo ""
 echo ""
 echo "DONE COMPILE"
 echo ""
 echo ""
-mvn -Dmaven.javadoc.skip=true verify && mvn clean install
+mvn clean install
 #mvn build
-ls -l
+ls -la
+artifact=find . -name "*.jar" -print0|xargs -r -0 ls -1 -t|head -1
+echo $artifact
